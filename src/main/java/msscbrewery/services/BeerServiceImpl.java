327ae78f6b98c2f68f437634d2f22e0a962df97f
GabrielGardev <gabriel.gurdev@gmail.com>
2020-09-02 16:33:46 +0300

    add post, put and delete method

--- src/main/java/msscbrewery/services/BeerServiceImpl.java
+++ src/main/java/msscbrewery/services/BeerServiceImpl.java
@@ -1,10 +1,12 @@
 package msscbrewery.services;
 
+import lombok.extern.slf4j.Slf4j;
 import msscbrewery.web.model.BeerDto;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Slf4j
 @Service
 public class BeerServiceImpl implements BeerService {
     @Override
@@ -13,5 +15,22 @@ public class BeerServiceImpl implements BeerService {
                 .beerName("Galaxy Cat")
                 .beerStyle("Pale Ale")
                 .build();
+    }
+
+    @Override
+    public BeerDto saveNewBeer(BeerDto beerDto) {
+        return BeerDto.builder()
+                .id(UUID.randomUUID())
+                .build();
+    }
+
+    @Override
+    public void updateBeer(UUID beerId, BeerDto beerDto) {
+        //TODO: would add a rest imp to update beer
+    }
+
+    @Override
+    public void deleteById(UUID beerId) {
+        log.debug("Deleting a bear...");
     }
 }
